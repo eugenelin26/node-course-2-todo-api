590ec0727d2ba63cd98bc4909a0244185b091f0b
eugenelin <eugenelin26@gmail.com>
2019-06-13 19:23:57 +0800

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -16,7 +16,6 @@ var app = express()
 app.use(bodyParser.json())
 
 // routes
-// Using postman
 app.post('/todos', (req, res) => {
   // console.log(req.body)
   var todo = new Todo({
@@ -27,6 +26,17 @@ app.post('/todos', (req, res) => {
   todo.save().then(
     doc => {
       res.send(doc)
+    },
+    e => {
+      res.status(400).send(e)
+    }
+  )
+})
+
+app.get('/todos', (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos })
     },
     e => {
       res.status(400).send(e)

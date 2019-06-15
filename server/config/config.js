87dffe8172cbd05752ea019cb900cad0fda64fa5
eugenelin <eugenelin26@gmail.com>
2019-06-16 01:52:28 +0800

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -3,10 +3,27 @@
 var env = process.env.NODE_ENV || 'development'
 // console.log('env ******', env)
 
-if (env === 'development') {
-  process.env.PORT = 3000
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
-} else if (env === 'test') {
-  process.env.PORT = 3000
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json')
+  var envConfig = config[env]
+
+  Object.keys(envConfig).forEach(key => {
+    process.env[key] = envConfig[key]
+  })
 }
+
+// if (env === 'development') {
+//   process.env.PORT = 3000
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
+// } else if (env === 'test') {
+//   process.env.PORT = 3000
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+// }
+
+// configure heroku, sample commands
+// heroku config // check
+// heroku config:set NAME=Eugene // add name (or any variable) eugene to heroku config
+// heroku config:get NAME // get name, in this case, Eugene
+// heroku config:unset NAME // remove name
+
+// need to set JWT_ENV
--- server/models/user.js
+++ server/models/user.js
@@ -53,7 +53,7 @@ UserSchema.methods.generateAuthToken = function() {
         _id: user._id.toHexString(),
         access
       },
-      'abc123'
+      process.env.JWT_SECRET
     )
     .toString()
 
@@ -81,7 +81,7 @@ UserSchema.statics.findByToken = function(token) {
   var decoded
 
   try {
-    decoded = jwt.verify(token, 'abc123')
+    decoded = jwt.verify(token, process.env.JWT_SECRET)
   } catch (e) {
     // return new Promise((resolve, reject) => {
     //   reject()

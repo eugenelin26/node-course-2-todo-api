2f7fdad9edf3c01a392be7696c2c74b6004d45a6
eugenelin <eugenelin26@gmail.com>
2019-06-15 17:43:55 +0800

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,7 @@ const { ObjectID } = require('mongodb')
 
 const { app } = require('./../server')
 const { Todo } = require('./../models/todo')
+const { User } = require('./../models/user')
 
 const { todos, populateTodos, users, populateUsers } = require('./seed/seed')
 
@@ -217,7 +218,20 @@ describe('POST /users', () => {
         expect(res.body._id).toBeTruthy()
         expect(res.body.email).toBe(email)
       })
-      .end(done)
+      .end(err => {
+        if (err) {
+          return done(err)
+        }
+
+        User.findOne({ email })
+          .then(user => {
+            // expect(user).toExist()
+            expect(user).toBeTruthy()
+            expect(user.password).not.toBe(password)
+            done()
+          })
+          .catch(e => done(e))
+      })
   })
 
   it('should return validation errors if request invalid', done => {
@@ -243,3 +257,69 @@ describe('POST /users', () => {
       .end(done)
   })
 })
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', done => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.header['x-auth']).toBeTruthy()
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        User.findById(users[1]._id)
+          .then(user => {
+            // expect(user.tokens[0]).toInclude()
+            expect(user.tokens[0]).toMatchObject({
+              access: 'auth',
+              token: res.headers['x-auth']
+            })
+            done()
+          })
+          .catch(e => done(e))
+      })
+  })
+
+  it('should reject invalid login', done => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: 'invalid password'
+      })
+      .expect(400)
+      .expect(res => {
+        expect(res.header['x-auth']).toBeFalsy()
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        User.findById(users[1]._id)
+          .then(user => {
+            // expect(user.tokens[0]).toInclude()
+            expect(user.tokens.length).toBe(0)
+            done()
+          })
+          .catch(e => done(e))
+      })
+  })
+})
+
+// describe('', () => {
+//   it('', done => {
+//     request(app)
+
+//       .end(done)
+//   })
+
+// })
